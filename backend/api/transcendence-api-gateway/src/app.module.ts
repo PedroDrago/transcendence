c4f1eea3f10b6a7f7a000e9e515c77493fc45ebd
fenol64 <contato@fenol64.com.br>
2026-01-02 19:51:43 -0300

    setup prometheus and grafana

--- backend/api/transcendence-api-gateway/src/app.module.ts
+++ backend/api/transcendence-api-gateway/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [],
+  imports: [PrometheusModule.register({
+    // Configure your Prometheus options here
+    defaultLabels: {
+      app: 'transcendence-api-gateway',
+    },
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
