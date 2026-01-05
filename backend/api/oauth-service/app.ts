df54991d19ce04d9aa962618321c83c04db24c10
fenol64 <fenol64.dev@gmail.com>
2026-01-05 15:39:06 -0300

    adding oauth service

--- backend/api/oauth-service/app.ts
+++ backend/api/oauth-service/app.ts
@@ -6,7 +6,10 @@ server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({
+  port: Number(process.env.PORT) || 8080,
+  host: '0.0.0.0'
+}, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
