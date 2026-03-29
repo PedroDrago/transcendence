e14ee3dc10163a6956d66aa31caff8db33a24ad1
gbmoraes-dev <dev.gbmoraes@gmail.com>
2026-03-28 22:08:28 -0300

    feat: add delete post routes

---
+++ backend/services/posts/src/http/routes/posts/delete-post.ts
@@ -0,0 +1,61 @@
+import { and, eq } from 'drizzle-orm'
+import { Elysia } from 'elysia'
+import { z } from 'zod'
+import { redis } from '@/cache'
+import { db } from '@/database'
+import { schemas } from '@/database/schemas'
+import { middlewares } from '@/http/middlewares'
+import { r2 } from '@/storage'
+
+export const deletePost = new Elysia().use(middlewares).delete(
+  '/post/:id',
+  async ({ userId, params, status }) => {
+    const { id } = params
+
+    const [deleted] = await db
+      .delete(schemas.posts)
+      .where(and(eq(schemas.posts.userId, userId), eq(schemas.posts.id, id)))
+      .returning()
+
+    if (!deleted) {
+      return status(404, {
+        error: 'Not found',
+        message: 'Post not found',
+      })
+    }
+
+    const postsKey = `posts:${id}`
+
+    const userPostsKey = `posts:user:${userId}`
+
+    const signedUrlKey = `signed-url:${deleted.mediaKey}`
+
+    await Promise.all([
+      r2.delete(deleted.mediaKey),
+      redis.del(signedUrlKey),
+      redis.del(postsKey),
+      redis.del(userPostsKey),
+    ])
+
+    return status(204, undefined)
+  },
+  {
+    auth: true,
+    detail: {
+      tags: ['Posts'],
+      summary: 'Delete a post',
+      description: 'Delete a post by its Id',
+      operationId: 'deletePost',
+    },
+    params: z.object({
+      id: z.string(),
+    }),
+    response: {
+      204: z.undefined(),
+      404: z.object({
+        error: z.string(),
+        message: z.string(),
+      }),
+    },
+  }
+)
