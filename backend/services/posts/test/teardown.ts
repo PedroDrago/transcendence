2e33d54386ae92b186ff252f538052284b07b232
gbmoraes-dev <dev.gbmoraes@gmail.com>
2026-03-30 19:59:13 -0300

    feat: add teardown helper

---
+++ backend/services/posts/test/teardown.ts
@@ -0,0 +1,18 @@
+import { sql } from 'drizzle-orm'
+import { redis } from '@/cache'
+import { db } from '@/database'
+import { r2 } from '@/storage'
+
+export async function teardown() {
+  await Promise.all([
+    db.execute(
+      sql`TRUNCATE TABLE posts.posts, posts.stories, posts.highlights, posts.highlight_stories RESTART IDENTITY CASCADE`
+    ),
+    redis.send('FLUSHDB', []),
+    r2
+      .list()
+      .then((objects) =>
+        Promise.all((objects.contents ?? []).map((obj) => r2.delete(obj.key)))
+      ),
+  ])
+}
