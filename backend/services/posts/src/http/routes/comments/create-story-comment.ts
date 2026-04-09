cb4c246d753b6fd10dc2bfb966813f49e53adb29
gbmoraes-dev <dev.gbmoraes@gmail.com>
2026-04-09 01:52:02 -0300

    feat: add create story comments route

---
+++ backend/services/posts/src/http/routes/comments/create-story-comment.ts
@@ -0,0 +1,63 @@
+import { eq } from 'drizzle-orm'
+import { Elysia, status } from 'elysia'
+import { z } from 'zod'
+import { redis } from '@/cache'
+import { db } from '@/database'
+import { schemas } from '@/database/schemas'
+import { commentInsertSchema } from '@/database/schemas/comments'
+import { middlewares } from '@/http/middlewares'
+
+export const createStoryComment = new Elysia().use(middlewares).post(
+  '/stories/:storyId/comments',
+  async ({ userId, params, body }) => {
+    const { storyId } = params
+    const { content } = body
+
+    const [story] = await db
+      .select({ id: schemas.stories.id })
+      .from(schemas.stories)
+      .where(eq(schemas.stories.id, storyId))
+
+    if (!story) {
+      return status(404, {
+        error: 'Not found',
+        message: 'Story not found',
+      })
+    }
+
+    const [comment] = await db
+      .insert(schemas.comments)
+      .values({
+        userId,
+        storyId,
+        content,
+      })
+      .returning()
+
+    await redis.del(`stories:${storyId}:comments`)
+
+    return status(201, { ...comment })
+  },
+  {
+    auth: true,
+    detail: {
+      tags: ['Comments'],
+      summary: 'Create a comment',
+      description: 'Create a new comment for the specified story',
+      operationId: 'createComment',
+    },
+    params: z.object({
+      storyId: z.string(),
+    }),
+    body: z.object({
+      content: z.string().min(1).max(2200),
+    }),
+    response: {
+      201: commentInsertSchema,
+      404: z.object({
+        error: z.string(),
+        message: z.string(),
+      }),
+    },
+  }
+)
