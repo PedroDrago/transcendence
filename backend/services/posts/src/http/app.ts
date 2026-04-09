4c8fa5237f06ba4571829257ea9baf55a9d17368
gbmoraes-dev <dev.gbmoraes@gmail.com>
2026-04-09 01:56:34 -0300

    feat: add comments routes in server

--- backend/services/posts/src/http/app.ts
+++ backend/services/posts/src/http/app.ts
@@ -2,6 +2,7 @@ import { treaty } from '@elysiajs/eden'
 import { Elysia } from 'elysia'
 import { middlewares } from './middlewares'
 import { plugins } from './plugins'
+import { comments } from './routes/comments'
 import { health } from './routes/health'
 import { media } from './routes/media'
 import { posts } from './routes/posts'
@@ -14,5 +15,6 @@ export const app = new Elysia()
   .use(media)
   .use(posts)
   .use(stories)
+  .use(comments)
 
 export const api = treaty(app)
